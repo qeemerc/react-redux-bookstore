d7e6e29d35337a5613082ddde66761337c4c44e3
Qeemerc <qeemerc@yandex.ru>
2018-06-14 13:40:04 +0300

    update mockapi

--- src/actions/bookPage.js
+++ src/actions/bookPage.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import * as actionTypes from '../actionTypes'
 
 export const fetchBookById = (id) => dispatch => {
-  axios('/books.json').then( ({data}) => {
+  axios.get('https://5b1f792317cc7000142ed492.mockapi.io/bookstore/books').then( ({data}) => {
       const book = data[id]
       dispatch({
         type: actionTypes.FETCH_BOOK_BY_ID,
